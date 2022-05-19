59aa3b4a2509aaff53ec1cfb34562c30e5b722fa
rmfkxhxh <manboong94@gmail.com>
2022-05-19 20:16:06 +0900

    feature : add chatting component

--- back/server.js
+++ back/server.js
@@ -13,7 +13,40 @@ app.use(cors({
 
 app.use(router);
 
-app.listen(9400, () => {
+const server = app.listen(9400, () => {
     console.log('http://localhost:9400')
 })
 
+const io = require('socket.io')(server,{
+    cors: { origin: '*' },
+})
+
+const chat = io.of('/chat')
+
+// console.log(chat)
+chat.on('connection', socket =>{
+    let c_id
+    socket.on('joinRoom', chatRoom => {
+        c_id = chatRoom
+        socket.join(chatRoom)
+        console.log('current_room : ',chatRoom)
+    })
+
+    socket.on('leaveRoom', chatRoom => {
+        socket.leave(chatRoom)
+        console.log('leave room : ', chatRoom)
+        
+    })
+
+    socket.on('message', (msg)=> {
+        const {author, data, c_id:curr_id} = msg
+        console.log(msg)
+
+        chat.to(curr_id).emit('send',msg)
+    })
+
+    socket.on('disconnect',()=>{
+        console.log(c_id,'번 나감~')
+        socket.leave(c_id)
+    })
+})
