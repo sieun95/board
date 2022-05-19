6bf1a6651e5d37315452ddeb40d59bb6ba17d704
sungjinWi <rkdlem94@gmail.com>
2022-05-19 19:46:39 +0900

    rough theme

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Main from "./component/Main"
+import LogIn from './component/LogIn';
+import SignUp from './component/SignUp';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path='/login' element={<LogIn />} />
+      <Route path='/signup' element={<SignUp />} />
+    </Routes>
+    </>
   );
 }
 
